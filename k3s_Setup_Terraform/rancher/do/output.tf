e3511d73201f55c2099cdcc1ac4a552f5cd27599
Boluwatife Morawo <olmofetoluwa@gmail.com>
2023-03-18 05:51:55 +0100

    fixed name conflicts in output.tf

--- k3s_Setup_Terraform/rancher/do/output.tf
+++ k3s_Setup_Terraform/rancher/do/output.tf
@@ -7,10 +7,10 @@ output "rancher_node_ip" {
   value = digitalocean_droplet.rancher_server.ipv4_address
 }
 
-output "workload_node_ip" {
+output "workload_1_node_ip" {
   value = digitalocean_droplet.cluster_node1.ipv4_address
 }
 
-output "workload_node_ip" {
+output "workload_2_node_ip" {
   value = digitalocean_droplet.cluster_node2.ipv4_address
 }
